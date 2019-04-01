02135ba6b316a74edac3caa218d016501e6534bc
v7Meluha <varshu.md@gmail.com>
2019-04-01 17:43:04 +0530

    Bot with Heroku

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ app.use(express.json());
 
 app.post("/", (req, res) => {
     var action = req.body.queryResult.action,
-    params = req.body.queryResult.parameters;
-      if (req.body.queryResult.hasOwnProperty('outputContexts')) {
-          params = req.body.queryResult.outputContexts
-      }
+        params = req.body.queryResult.parameters;
+    if (req.body.queryResult.hasOwnProperty('outputContexts')) {
+        params = req.body.queryResult.outputContexts
+    }
 
     return Bot.getResponse(action, params)
-    .then(function(message) {
-        res.json(message);
-        console.log("\n---Request serviced---\n")
-    })
-    .catch((err)=>{
-      res.json("Oops..couldn't find anything for that. Try a mroe specific query")
-    })
+        .then(function(message) {
+            res.json(message);
+            console.log("\n---Request serviced---\n")
+        })
+        .catch((err) => {
+            res.json("Oops..couldn't find anything for that. Try a mroe specific query")
+        })
 })
-app.listen(7000, () => console.log("starting server"));
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
